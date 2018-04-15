6f59255756b7a7ffb4cc1d234b4493d7b2410cc4
Kate Jeziorska <k_jeziorska@yahoo.com>
2018-04-15 11:36:29 -0700

    Add imgs | first lines of js | card-container part of html

--- js/main.js
+++ js/main.js
@@ -4,4 +4,16 @@ function downloadURI(uri, name) {
   link.href = uri;
   link.click();
 }
+var cardContainer = document.getElementById('card-container');
+var cardImage = document.getElementById('card-image');
+var cardTitle = document.getElementById('card-title');
+var cardMsg = document.getElementById('card-msg');
+var cardFrom = document.getElementById('card-from');
 
+
+var formTitle = document.getElementById('title');
+var formFrom = document.getElementById('from');
+var formMsg = document.getElementById('msg');
+var formImage = document.getElementById('image');
+
+var saveButton = document.getElementById('save-button');
